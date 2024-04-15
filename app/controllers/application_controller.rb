58351b772e552a10bf4f2d1c875c228b08606314
John Gallagher <john@synapticmishap.co.uk>
2024-04-15 21:50:14 +0100

    fix: add sidekiq

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
 class ApplicationController < ActionController::Base
-  # def append_info_to_payload(payload)
-  #   super
-  #   payload.merge!(
-  #     http: {
-  #       response: {
-  #         status: response.status
-  #       }
-  #     }
-  #   )
-  # end
 end
