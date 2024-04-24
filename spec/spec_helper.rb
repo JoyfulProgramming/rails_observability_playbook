8cde2bdabb7aab1ed741dd2627da40d55f8786b4
John Gallagher <john@synapticmishap.co.uk>
2024-04-24 15:32:29 +0100

    fix: remove useless distracting gems in backtraces

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.backtrace_exclusion_patterns << %r{/gems/}
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.disable_monkey_patching!
   config.order = :random
