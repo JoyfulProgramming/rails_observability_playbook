4b6d77d38835c2248766f7c0b1e08d5068a0da1a
John Gallagher <john@synapticmishap.co.uk>
2024-04-24 20:44:53 +0100

    fix: link child spans

--- config/initializers/active_job.rb
+++ config/initializers/active_job.rb
@@ -2,13 +2,46 @@
 
 require 'opentelemetry/sdk'
 
+# rubocop:disable Metrics/BlockLength
 OpenTelemetry::SDK.configure do |c|
-  c.use_all
+  c.use 'OpenTelemetry::Instrumentation::Rails'
   c.use 'OpenTelemetry::Instrumentation::ActiveJob', {
     span_naming: :job_class,      # Options are :job_class or :queue
     force_flush: true,            # Options are true or false
     propagation_style: :child     # Options are :link, :child, or :none
   }
+  c.use 'OpenTelemetry::Instrumentation::ActiveSupport'
+  c.use 'OpenTelemetry::Instrumentation::Rack', {
+    allowed_request_headers: [],
+    allowed_response_headers: [],
+    application: nil,
+    record_frontend_span: false,
+    untraced_endpoints: [],
+    url_quantization: nil,
+    untraced_requests: nil,
+    response_propagators: [],
+    use_rack_events: true,
+    allowed_rack_request_headers: {},
+    allowed_rack_response_headers: {}
+  }
+
+  c.use 'OpenTelemetry::Instrumentation::ActionPack'
+  c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
+  c.use 'OpenTelemetry::Instrumentation::ActionView', {
+    disallowed_notification_payload_keys: [], notification_payload_transform: nil
+  }
+  c.use 'OpenTelemetry::Instrumentation::ConcurrentRuby'
+  c.use 'OpenTelemetry::Instrumentation::Faraday', { span_kind: :client, peer_service: nil }
+  c.use 'OpenTelemetry::Instrumentation::Net::HTTP', { untraced_hosts: [] }
+  c.use 'OpenTelemetry::Instrumentation::PG',
+        { peer_service: nil, db_statement: :obfuscate, obfuscation_limit: 2000 }
+  c.use 'Instrumentation: OpenTelemetry::Instrumentation::Rails'
+  c.use 'OpenTelemetry::Instrumentation::Rake'
+  c.use 'OpenTelemetry::Instrumentation::Redis',
+        { peer_service: nil, trace_root_spans: true, db_statement: :obfuscate }
+  c.use 'OpenTelemetry::Instrumentation::Sidekiq',
+        { span_naming: :queue, propagation_style: :link, trace_launcher_heartbeat: false,
+          trace_poller_enqueue: false, trace_poller_wait: false, trace_processor_process_one: false, peer_service: nil }
 
   if Rails.env.test?
     c.logger = Logger.new(IO::NULL)
@@ -25,6 +58,7 @@ OpenTelemetry::SDK.configure do |c|
     )
   end
 end
+# rubocop:enable Metrics/BlockLength
 
 module Observable
   module Instrumentation
