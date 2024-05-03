daed11f5bc4713d68b164d5c092bf29e396e7fc1
John Gallagher <john@synapticmishap.co.uk>
2024-05-03 20:13:20 +0100

    feat: upgrade library and set max retries to 3 to test

--- app/jobs/refresh_todos_job.rb
+++ app/jobs/refresh_todos_job.rb
@@ -3,7 +3,7 @@
 # This class represents a job that refreshes todos.
 class RefreshTodosJob
   include Sidekiq::Job
-  sidekiq_options queue: :within_five_minutes
+  sidekiq_options queue: :within_five_minutes, retry: 3
 
   # Performs the job by calling the RefreshTodos service.
   def perform
