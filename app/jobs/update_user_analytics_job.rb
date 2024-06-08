4866f132a4ca6a4a81b26360a3b21adf5d9eaaf1
John Gallagher <john@synapticmishap.co.uk>
2024-06-08 17:40:35 +0100

    fix: cleaner

--- app/jobs/update_user_analytics_job.rb
+++ app/jobs/update_user_analytics_job.rb
@@ -2,8 +2,6 @@ class UpdateUserAnalyticsJob
   include Sidekiq::Job
 
   def perform(user_id)
-    user = User.find(user_id)
-    user.increment!(:number_of_profile_visits)
-    user.save!
+    User.find(user_id).increment!(:number_of_profile_visits)
   end
 end
