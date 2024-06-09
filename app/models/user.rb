1bb3628aa0e48f47493fc570f9b0194d68ab2d64
John Gallagher <john@synapticmishap.co.uk>
2024-06-09 21:58:22 +0100

    feat: add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable, :confirmable
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
