4d16470259f081449c25a61d4c779bcf420f1043
John Gallagher <john@synapticmishap.co.uk>
2024-06-10 02:41:18 +0100

    wip: try the TCP settings again

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -4,8 +4,8 @@ Rails.application.config.after_initialize do
   if ENV["DISABLE_DATADOG_AGENT"].blank? && !Rails.env.local?
     Rails.application.configure do
       config.semantic_logger.add_appender(
-        appender: :http,
-        server: "127.0.0.1:443",
+        appender: :tcp,
+        server: "127.0.0.1:10518",
         formatter: :json
       )
     end
