f825788f12c1e311dc8084f9b929f24cc1d1a922
John Gallagher <john@synapticmishap.co.uk>
2024-06-10 02:43:09 +0100

    fix: don't halt server starting

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,12 +2,16 @@
 
 Rails.application.config.after_initialize do
   if ENV["DISABLE_DATADOG_AGENT"].blank? && !Rails.env.local?
-    Rails.application.configure do
-      config.semantic_logger.add_appender(
-        appender: :tcp,
-        server: "127.0.0.1:10518",
-        formatter: :json
-      )
+    begin
+      Rails.application.configure do
+        config.semantic_logger.add_appender(
+          appender: :tcp,
+          server: "127.0.0.1:10518",
+          formatter: :json
+        )
+      end
+    rescue Net::TCPClient::ConnectionFailure
+      Rails.logger.warn "Failed to connect to Datadog Agent"
     end
   end
 end
