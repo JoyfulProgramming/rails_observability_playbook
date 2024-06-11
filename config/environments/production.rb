c2302dc9776942848abec3f38dafdf8ce5890510
John Gallagher <john@synapticmishap.co.uk>
2024-06-11 20:43:13 +0100

    fix: don't hardcode logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,9 +46,6 @@ Rails.application.configure do
   # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
 
-  # Prepend all log lines with the following tags.
-  config.log_tags = [:request_id]
-
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
